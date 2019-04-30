ce10568f7cb765776da3568564de4c2950bd62c2
Dan Berdikulov <danberdov@gmail.com>
2019-04-30 21:51:07 +0600

    update configs

--- config.py
+++ config.py
@@ -4,19 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    SELENIUM_OPTIONS=False
+    SELENIUM_BOOST_OPTIONS=False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(Config):
-    DEBUG=False
-    SELENIUM_OPTIONS=True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-    SELENIUM_OPTIONS=True
-
+    SELENIUM_BOOST_OPTIONS=True
 
 class TestingConfig(Config):
     TESTING = True
