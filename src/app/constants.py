75b54484d0e2715e22f72b213f02c238f489a99b
Dan Berdikulov <danberdov@gmail.com>
2019-05-02 20:34:08 +0600

    edit constants

--- src/app/constants.py
+++ src/app/constants.py
@@ -1,10 +1,11 @@
-class Ad:
+class Business:
     TITLE = 'title'
     NUMBERS = 'numbers'
     ADDRESS = 'address'
     INSTAGRAM = 'instagram'
     EMAIL = 'email'
     WEBSITE = 'website'
+    HTML = 'html'
 
 PAGE_URL = 'https://2gis.kg'
 PAGE_URL_SCELETON = 'https://2gis.kg/bishkek/search/{}/{}' 
