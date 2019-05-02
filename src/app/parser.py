1632069c294a33de3b520d35dac277eed56eff33
Dan Berdikulov <danberdov@gmail.com>
2019-05-02 20:34:29 +0600

    edit parser

--- src/app/parser.py
+++ src/app/parser.py
@@ -1,20 +1,21 @@
 import os
 import unicodedata
+import sys
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 from . import settings
-from .constants import Ad
+from .constants import Business
 from . import constants
+import config
 
 
 class Parser:
 
     def __init__(self, parser_booster=False):
         driver_options = settings.options if parser_booster else None
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=driver_options)
+        self.driver = webdriver.Chrome(config.chrome_driver_path, chrome_options=driver_options)
         self.category_page = 1
         self.businesses = []
 
@@ -52,20 +53,20 @@ class Parser:
         soup = BeautifulSoup(html, 'html.parser')
         business = {}
         title = soup.find('h1')
-        business[Ad.TITLE] = title.text if title else None
+        business[Business.TITLE] = title.text if title else None
         numbers = soup.find_all('a', href = lambda x: x and 'tel:' in x)
-        business[Ad.NUMBERS] = list(set([number['href'][4:] for number in numbers]))
+        business[Business.NUMBERS] = list(set([number['href'][4:] for number in numbers]))
         address = soup.find('a', lambda x: x and 'address' in x)
-        business[Ad.ADDRESS] = unicodedata.normalize("NFKD", address.text) if address else None
+        business[Business.ADDRESS] = unicodedata.normalize("NFKD", address.text) if address else None
         instagram = soup.find('a', lambda x: x and 'instagram' in x)
-        business[Ad.INSTAGRAM] = instagram['href'] if instagram else None
+        business[Business.INSTAGRAM] = instagram['href'] if instagram else None
         email = soup.find('a', href = lambda x: x and 'mailto:' in x)
-        business[Ad.EMAIL] = email.text if email else None
+        business[Business.EMAIL] = email.text if email else None
         website = soup.find('a', lambda x: x and 'website' in x) or \
             soup.find('a', lambda x: x and 'contact' in x,\
                 href = lambda x: x and ('http' in x or 'https' in x))
         website_url = website['href'].split("://") if website else None 
-        business[Ad.WEBSITE] = website_url[-1] if website_url \
+        business[Business.WEBSITE] = website_url[-1] if website_url \
             and len(website_url) > 2 else None
-        print(business)
+        business[Business.HTML] = soup.find('body')
         return business
