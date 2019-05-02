93a3a6b24f9ea1635a090b72fea8eee05add0d96
Dan Berdikulov <danberdov@gmail.com>
2019-05-03 00:04:02 +0600

    update config

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 chrome_driver_path = os.path.join(basedir, 'chrome_driver', 'chromedriver')
+
+database_url = os.environ.get('DATABASE_URL')
+
+categories_path = os.path.join(basedir, 'data', 'categories.json')
+
