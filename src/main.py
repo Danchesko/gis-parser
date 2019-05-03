a4f89144140106ec7f57b04772718ee39137755f
Dan Berdikulov <danberdov@gmail.com>
2019-05-03 17:04:24 +0600

    update main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,4 @@
-import json
-import os
+from src.app import app
 
-import config
-from src.app import parser
-
-CATEGORIES_PATH = os.path.join(config.basedir, 'data', 'categories.json')
-
-    
-
-if __name__ == '__main__':
-    with open(CATEGORIES_PATH) as json_file:
-        data = json.load(json_file)
-    categories = data['all_categories']
-    parser_booster = True if os.environ.get('env') == 'prod' else False
-    parser = parser.Parser(parser_booster)
-    parser.parse_category('Телеканалы')
-
-#test
+if __name__ == "__main__":
+    app.run()
