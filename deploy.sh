c15ad914a2c9746b73f3d4ca52f9566f611beecf
Dan Berdikulov <danberdov@gmail.com>
2019-05-11 04:55:34 +0600

    edit deployer

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,20 @@ docker image rm $(docker image ls -q $project_name | uniq)
 echo building new image 
 docker build -t $project_name .
 
-echo stop running selenium 
+echo delete selenium if exists
 docker container rm -f `docker ps -a -q  --filter ancestor=$selenium`
 
-echo running selenium standalone chrom
+echo running selenium standalone chrome
 docker run -p 4444:4444 -d $selenium
 
+echo wait for selenium to run
+sleep 10
+
 echo running project 
-docker run --name=$project_name -d $project_name 
+while ! docker run --name=$project_name --net=host -it --rm $project_name
+do
+    echo apparently selenium crashed, booting it up again
+    docker container rm -f `docker ps -a -q  --filter ancestor=$selenium`
+    docker run -p 4444:4444 -d $selenium
+    sleep 10
+done
